25e6ccc5a0a7a60d491ddfe455928a575910befd
maheshbasnet089 <basnetmanish089@gmail.com>
2024-11-18 11:19:49 +0545

    first commit

---
+++ day1.js
@@ -0,0 +1,40 @@
+
+var bottle = "paani"
+let cup = "coffee"
+let age = 22
+let isNepali = true
+let isIndian = false
+
+console.log(bottle + cup)
+console.log(`${bottle + "is " + cup}`)
+// output --> My age is 22
+console.log("My age is " + age)
+console.log(`My age is ${age}`)
+
+// array
+let mobile1 = "Redmi"
+let mobile2 = "Iphone"
+let mobile3 = "Poco"
+
+let mobiles = ["asdf","Iphone","Poco"]
+console.log(mobiles[0])
+console.log(mobiles[1])
+console.log(mobiles[2])
+
+
+
+
+
+// euta category ko data xa vane chai array use garne 
+//objects
+let myName = "Manish"
+let myage = 22 
+let myAddress = "Balgram"
+
+let me = {
+    myName : "Manish", 
+    myage : 22, 
+    myAddress : "Balgram"
+}
+
+// sabai data euta sanga linked huncha taba object use garne 
